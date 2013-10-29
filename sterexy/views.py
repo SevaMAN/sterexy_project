9b01d8ea1d8378fa4848da544a73d14f92f637c1
SevaMAN <sevavsevolod@gmail.com>
2013-10-30 01:06:37 +0200

    get background from NASA

--- sterexy/views.py
+++ sterexy/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import urllib2
+import lxml.html
 
 def index (request):
-    return render(request, 'index.html')
+    page = urllib2.urlopen('http://apod.nasa.gov/apod/')
+    doc = lxml.html.document_fromstring(page.read())
+    href = doc.xpath('/html/body/center[1]/p[last()]/a')[0].get('href')
+    return render(request, 'index.html', {'href':href})
+    
