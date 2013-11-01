a2f111f81dc0353161c6226fdcd3076cdaf13d1b
Vsevolod Syrovatskiy <vsyrovatskiy@hortonworks.com>
2013-11-01 21:21:34 +0200

    collect and analyze background

--- sterexy/models.py
+++ sterexy/models.py
@@ -1,4 +1,23 @@
-from django.db import models as md
+from django.db import models
+from django.core.files import File
+import os
+import urllib
 
-class Mu(md.Model):
-    field = md.CharField(max_length=30)
+
+class Background(models.Model):
+
+    class Meta:
+        get_latest_by = 'im_date'
+
+    image_file = models.ImageField(upload_to='sterexy/static/images/')
+    image_url = models.URLField()
+    im_date = models.DateField()
+
+    def get_remote_image(self):
+        if self.image_url and not self.image_file:
+            result = urllib.urlretrieve(self.image_url)
+            self.image_file.save(
+                    'background.jpg',
+                    File(open(result[0]))
+                    )
+            self.save()
--- sterexy/views.py
+++ sterexy/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 import urllib2
 import lxml.html
+from models import Background
+import time
 
 def index (request):
     page = urllib2.urlopen('http://apod.nasa.gov/apod/')
     doc = lxml.html.document_fromstring(page.read())
     href = doc.xpath('/html/body/center[1]/p[last()]/a')[0].get('href')
+
+    try:
+        a = Background.objects.latest()
+        a.delete()
+    except ObjectDoesNotExist:
+        bg = Background(image_url='http://apod.nasa.gov/apod/'+href)
+        picdate = doc.xpath('/html/body/center[1]/p[last()]')[0].text_content().replace('\n','')
+        picdate = time.strptime(picdate, "%Y %B %d")
+        bg.im_date = time.strftime("%Y-%m-%d",picdate)
+        bg.get_remote_image()
+
     return render(request, 'index.html', {'href':href})
     
